b564be1e0c7348a9b0dd5589529cad3ddf980399
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-09 22:26:21 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
     pig_latinized_string = ""
     words = text.split(" ")
     words.each_with_index do |word, i|
-      if i == 0
+      if i != words.length
         letter_count = 0
         while !word[letter_count].match(/[aeiouAEIOU]/) || word.length == letter_count
           letter_count += 1
@@ -15,6 +15,15 @@ class PigLatinizer
         end
       end
       elsif i == words.length
+        letter_count = 0
+        while !word[letter_count].match(/[aeiouAEIOU]/) || word.length == letter_count
+          letter_count += 1
+        end
+        if letter_count == 0
+          pig_latinized_string += word + "way"
+        else
+        pig_latinized_string += word[letter_count..word.length] + word[0..letter_count-1] + "ay"
+        end
     end
     pig_latinized_string
   end
