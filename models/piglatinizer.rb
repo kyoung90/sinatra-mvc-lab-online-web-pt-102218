68fcdef5459083bf6c129be05c434768936aa058
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-09 22:16:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinizer
     words = text.split(" ")
     words.each do |word|
       letter_count = 0
-      while !word[letter_count].match(/[aeiouAEIOU]/)
+      while !word[letter_count].match(/[aeiouAEIOU]/) || word.length == letter_count
         letter_count += 1
       end
-
+      
     end
   end
 end
