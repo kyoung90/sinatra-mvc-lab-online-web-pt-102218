04617046b13b662965c39b85cf2815d22a0c6849
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-09 22:11:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,8 +2,8 @@ class PigLatinizer
   def piglatinize(text)
     pig_latinized_string = " "
     words = text.split(" ")
-    words.each do |word| 
+    words.each do |word|
       letter_count = 0
-    end 
+    end
   end
 end
