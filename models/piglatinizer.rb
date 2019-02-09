565649e2d1ce7f5b7931b4853b43a8085d85ed06
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-09 22:25:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,16 +2,19 @@ class PigLatinizer
   def piglatinize(text)
     pig_latinized_string = ""
     words = text.split(" ")
-    words.each_with_index do |word|
-      letter_count = 0
-      while !word[letter_count].match(/[aeiouAEIOU]/) || word.length == letter_count
-        letter_count += 1
-      end
-      if letter_count == 0
-        pig_latinized_string += word + "way"
-      else
-      pig_latinized_string += word[letter_count..word.length] + word[0..letter_count-1] + "ay"
-      end
+    words.each_with_index do |word, i|
+      if i == 0
+        letter_count = 0
+        while !word[letter_count].match(/[aeiouAEIOU]/) || word.length == letter_count
+          letter_count += 1
+        end
+        if letter_count == 0
+          pig_latinized_string += word + "way "
+        else
+        pig_latinized_string += word[letter_count..word.length] + word[0..letter_count-1] + "ay "
+        end
+      end 
+      elsif i == words.length 
     end
     pig_latinized_string
   end
