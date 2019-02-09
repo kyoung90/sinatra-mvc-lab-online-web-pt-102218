4c68cc941c3725d9a60c94d4db31e5e4b7d5ec7d
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-09 22:07:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+  get "/" do
+    erb :user_input
+  end
+
+   post "/piglatinize" do
+    @user_phrase = params[:user_phrase]
+    "piglatinizied text"
+  end
 end
